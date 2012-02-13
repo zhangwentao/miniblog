8a7b7aa47b0769a1aeedfd9d3b7317bbaff867bd
wentao.zhang <taowenzhang@gmail.com>
2012-02-13 20:47:37 +0800

    change mysql settings for local

--- settings.py
+++ settings.py
@@ -13,9 +13,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wentao_blog',                      # Or path to database file if using sqlite3.
-        'USER': 'wentaome',                      # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'wentao',                  # Not used with sqlite3.
-        'HOST': 'mysql.wentao.me',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
